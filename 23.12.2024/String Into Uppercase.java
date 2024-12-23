672c7ec06ccc6f1deea7600612630f8839ae94a5
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-23 16:58:07 +0530

    Create String Into Uppercase.java

---
+++ 23.12.2024/String Into Uppercase.java
@@ -0,0 +1,7 @@
+public class UppercaseConversion {
+    public static void main(String[] args) {
+        String str = "hello world";
+        String upperStr = str.toUpperCase();
+        System.out.println("Uppercase: " + upperStr);
+    }
+}
