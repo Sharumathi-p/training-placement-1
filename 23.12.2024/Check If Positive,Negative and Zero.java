05d3aacab41ea1a2b605d1a2ac8ffbda8d34b5a1
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-23 16:38:48 +0530

    Create Check If Positive,Negative and Zero.java

---
+++ 23.12.2024/Check If Positive,Negative and Zero.java
@@ -0,0 +1,12 @@
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num = -10;
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+    }
+}
