c986fcabfb50d74a26bc6202cc3deed1fa6003b9
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-23 16:53:07 +0530

    Create Largest Of Two Numbers.java

---
+++ 23.12.2024/Largest Of Two Numbers.java
@@ -0,0 +1,11 @@
+public class LargestOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        if (num1 > num2) {
+            System.out.println(num1 + " is larger.");
+        } else {
+            System.out.println(num2 + " is larger.");
+        }
+    }
+}
