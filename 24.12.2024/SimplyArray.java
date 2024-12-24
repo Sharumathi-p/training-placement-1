2d8bdb42c8cfca6deb6240c78329900f8e25d8b2
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-24 12:51:01 +0530

    Create SimplyArray.java

---
+++ 24.12.2024/SimplyArray.java
@@ -0,0 +1,10 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        System.out.println("Array elements:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
