d7aeadc408cb7e5b19b7054de4996d0ed3284b39
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2024-12-24 12:49:11 +0530

    Create BasicArithmetic.java

---
+++ 24.12.2024/BasicArithmetic.java
@@ -0,0 +1,18 @@
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("Difference: " + (num1 - num2));
+        System.out.println("Product: " + (num1 * num2));
+        System.out.println("Quotient: " + (num1 / num2));
+        
+        scanner.close();
+    }
+}
